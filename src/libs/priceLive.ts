545ada5b1680e58754046e3d92c6c3c2c9984e8b
Zarin Rashid <zarinspeaks@gmail.com>
2025-08-14 00:12:25 -0600

    Refactor priceLive function for improved readability and formatting

--- src/libs/priceLive.ts
+++ src/libs/priceLive.ts
@@ -1,32 +1,32 @@
-import { ChatInputCommandInteraction} from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 import prisma from "../utils/prisma";
 import { getDevPrice } from "../utils/uniswapPrice";
 
-export default async function priceLive(interaction: ChatInputCommandInteraction){
-    try{
-        await interaction.deferReply();
+export default async function priceLive(
+  interaction: ChatInputCommandInteraction
+) {
+  try {
+    await interaction.deferReply();
 
     const liveprice = await getDevPrice();
 
-    if(!liveprice){
-        return await interaction.editReply("Failed to fetch live price");
+    if (!liveprice) {
+      return await interaction.editReply("Failed to fetch live price");
     }
 
-    await interaction.editReply(
-        `Live DEV Price: ${liveprice.toFixed(5)}`
-    );
+    await interaction.editReply(`Live DEV Price: ${liveprice.toFixed(5)}`);
 
     const savePrice = await prisma.tokenPrice.create({
-        data:{
-            price: liveprice,
-            tokenId: "scout-protocol-token",
-        }
+      data: {
+        price: liveprice,
+        tokenId: "scout-protocol-token",
+      },
     });
     return savePrice;
-} catch (error) {
-    console.log(error)
+  } catch (error) {
+    console.log(error);
     await interaction.editReply(
-        "Sorry, there was an error fetching the live price. Please try again later."
+      "Sorry, there was an error fetching the live price. Please try again later."
     );
   }
 }
